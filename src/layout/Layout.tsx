07eb874e8294674a3aa6087ec07f181c8f701b5b
Ankit Acharjee <ankitacharjee02@gmail.com>
2024-06-12 15:46:46 +0530

    chore: Add Layout component for consistent page structure

---
+++ src/layout/Layout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router-dom";
+import Navbar from "@/components/Navbar";
+import Sidebar from "@/components/Sidebar";
+
+const Layout = () => {
+  return (
+    <div className="flex flex-row h-screen w-full">
+      <Sidebar />
+      <main className="flex flex-col w-full h-full">
+        <Navbar />
+        <Outlet />
+      </main>
+    </div>
+  );
+};
+
+export default Layout;
