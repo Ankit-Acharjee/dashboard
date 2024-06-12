cf5df652d12b8aa9014ee3c3a415c770ed19847c
Ankit Acharjee <ankitacharjee02@gmail.com>
2024-06-12 15:46:17 +0530

    Add new pages for Activity, Analytics, Dashboard, Documents, Messages, and Wallet

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,12 @@
+import Investments from "@/components/Investments"
+
+const Dashboard = () => {
+    return (
+      <div>
+        <Investments />
+        
+      </div>
+    )
+  }
+  
+  export default Dashboard
