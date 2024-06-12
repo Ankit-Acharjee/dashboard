d60619268228caf31c59b5d3c827e3acd3bc95f8
Ankit Acharjee <ankitacharjee02@gmail.com>
2024-06-12 15:47:08 +0530

    feat: Add new SVG assets for Broadcom, Microsoft, Nvidia, and React logos

---
+++ src/icons/MicrosoftIcon.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+
+const MicrosoftIcon: React.FC = () => {
+  return (
+    <div>
+      <svg
+        version="1.1"
+        viewBox="0 0 1600 1600"
+        width="30"
+        height="30"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          transform="translate(842,842)"
+          d="m0 0h758v758h-758v-758z"
+          fill="#000002"
+        />
+        <path
+          transform="translate(0,842)"
+          d="m0 0h758v758h-758v-758z"
+          fill="#000002"
+        />
+        <path
+          transform="translate(842)"
+          d="m0 0h758v758h-758v-758z"
+          fill="#000002"
+        />
+        <path
+          transform="translate(0)"
+          d="m0 0h758v758h-758v-758z"
+          fill="#000002"
+        />
+      </svg>
+    </div>
+  );
+};
+
+export default MicrosoftIcon;
